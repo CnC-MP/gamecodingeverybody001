6e3ab34a8ac9b739508863c9e880d2e70e25ca7c
nohheekang <nohheekang@gmail.com>
2020-05-26 22:20:27 +0900

    dd

--- ShootingGame/ShootingGame/소스.cpp
+++ ShootingGame/ShootingGame/소스.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "interface_design_tool.h"
+#include <conio.h>
 
 int main() {
-	return 0;
+	system("mode con:cols=45 lines=30");
+	
+	gotoxy(18, 19);
+	color_change(14);
+	printf("게임 시작");
+	_getch();
 }
